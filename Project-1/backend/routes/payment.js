5d99ee2d6108dc0ddaa7528c4de6fdb3459f0e8b
Javeedkhan8 <javeedkhanjohnbasha8@gmail.com>
2024-12-26 18:52:00 +0530

    backend

--- Project-1/backend/routes/payment.js
+++ Project-1/backend/routes/payment.js
@@ -15,7 +15,7 @@ const paypalClient = new paypal.core.PayPalHttpClient(paypalEnvironment);
 
 // Razorpay setup
 const razorpayInstance = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
+  key_id:process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
